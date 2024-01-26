1d9c840592811dce343bc811ded0967ee46e4745
stroich <stroichmwork@gmail.com>
2024-01-26 20:04:16 +0400

    feat: add a transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,12 @@
+import  { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+    const reverseStream = new Transform({
+        transform(chunk, encoding, callback) {
+            callback(null, chunk.toString().split('').reverse().join('') + '\n');
+        },
+    });
+    process.stdin.pipe(reverseStream).pipe(process.stdout);
 };
 
 await transform();
