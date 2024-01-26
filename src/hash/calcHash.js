4315f690cc6467a862dcf74633bbe2a4bea1fa5a
stroich <stroichmwork@gmail.com>
2024-01-26 19:51:52 +0400

    feat: add a calculateHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import fs from 'fs';
+import crypto from "crypto";
+import {join, dirname} from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+    const pathFile = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+    const stream = fs.createReadStream(pathFile);
+    const hash = crypto.createHash('sha256');
+
+    stream.on('data', data => {
+        hash.update(data);
+    });
+
+    stream.on('end', () => {
+        const hexData = hash.digest('hex');
+        console.log(hexData);
+    });
 };
 
 await calculateHash();
