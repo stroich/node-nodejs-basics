9ab8a716cf844868351942e6b21c2c72230856e7
stroich <stroichmwork@gmail.com>
2024-01-26 19:37:54 +0400

    feat: add a copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,36 @@
+import fs from 'fs';
+import path, {dirname} from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const projectFolderCopy = path.join(__dirname, 'files-copy');
+const projectFolder = path.join(__dirname, 'files');
+
 const copy = async () => {
-    // Write your code here 
+    fs.promises.access(projectFolderCopy, fs.constants.R_OK).then(() => {
+        throw new Error('FS operation failed');
+    }).catch((err) => {
+        if (err.code === 'ENOENT') {
+            fs.promises.readdir(projectFolder)
+                .catch(() => {
+                    throw new Error('FS operation failed');
+                })
+                .then((files) => {
+                    fs.promises.mkdir(projectFolderCopy)
+                        .then(() => {
+                            files.forEach((file) => {
+                                const projectFile = path.join(projectFolder, file);
+                                const projectFileCopy = path.join(projectFolderCopy, file);
+                                fs.promises.copyFile(projectFile, projectFileCopy);
+                            });
+                        })
+                })
+        } else {
+            throw new Error('FS operation failed');
+        }
+    })
 };
 
 await copy();
