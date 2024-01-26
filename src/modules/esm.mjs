e79daae9b0c287017b9c31233bfacaa39160b03e
stroich <stroichmwork@gmail.com>
2024-01-26 20:02:20 +0400

    refactor: rewrite esm.mjs

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,16 +1,20 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path, {dirname} from 'path';
+import {release, version} from 'os';
+import {createServer as createServerHttp} from 'http';
+import { fileURLToPath } from 'url';
+import './files/c.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const random = Math.random();
 
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+    unknownObject = await import('./files/a.json', { with: { type: 'json' } })
 } else {
-    unknownObject = require('./files/b.json');
+    unknownObject = await import('./files/b.json', { with: { type: 'json' } })
 }
 
 console.log(`Release ${release()}`);
@@ -33,8 +37,8 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
+export {
     unknownObject,
-    myServer,
+    myServer
 };
 
