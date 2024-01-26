a20e4e77b2a2dff770a04cbfd3156af85d072d92
stroich <stroichmwork@gmail.com>
2024-01-26 23:44:03 +0400

    feat: add worker

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,34 @@
+import {Worker, isMainThread} from 'worker_threads';
+import os from 'os';
+import path, {dirname} from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const performCalculations = async () => {
-    // Write your code here
+    const pathWorker = path.join(__dirname, 'worker.js');
+    const numCores = os.cpus().length;
+    const workers = [];
+
+    if (isMainThread) {
+        for (let i = 0; i < numCores; i++) {
+            const promise = new Promise((resolve) =>{
+                const uniqueData = 10 + i;
+                const worker = new Worker(pathWorker, { workerData: uniqueData });
+                worker.on('message', (msg)=>{
+                    resolve({ status: 'resolved', data: msg })
+                })
+                worker.on('error', ()=>{
+                    resolve({ status: 'error', data: null })
+                })
+            })
+            workers.push(promise);
+        }
+    }
+
+    const result = await Promise.all(workers);
+    console.log(result);
 };
 
 await performCalculations();
