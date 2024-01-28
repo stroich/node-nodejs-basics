61576cdea36ea9692ec8043047064cd51e17657b
stroich <stroichmwork@gmail.com>
2024-01-28 13:24:04 +0400

    feat: add a parseEnv function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,8 @@
 const parseEnv = () => {
-    // Write your code here 
+    const envData = process.env;
+    const RSSArray = Object.entries(envData).filter(([key])=>key.startsWith('RSS_'));
+    const RSSString = RSSArray.map((el)=>`${el[0]}=${el[1]}`).join('; ')
+    console.log(RSSString);
 };
 
 parseEnv();
